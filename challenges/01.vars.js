2987375ee9f5aaccd9775f925e196acd92b0301a
alexBlocks <109334902+alexBlocks@users.noreply.github.com>
2023-06-07 22:37:07 +1200

    Initial upload

---
+++ challenges/01.vars.js
@@ -0,0 +1,10 @@
+let firstName = "Alex",
+  last_name = "Pastukhov";
+// firstName = "Alex";
+// last_name = "Pastukhov";
+
+let address = "1 Main St";
+address = "2 Wall St";
+
+console.log(firstName, last_name);
+console.log(address);
